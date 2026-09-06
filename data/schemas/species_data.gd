80d326782b3200d1c2b39c5e553eab7171300ec1
Lucas Motizuki <motizukilucas@gmail.com>
2026-09-06 19:18:47 +0100

    add: base species resource

---
+++ data/schemas/species_data.gd
@@ -0,0 +1,31 @@
+class_name SpeciesData
+extends Resource
+
+enum Type {
+	NONE,
+	NORMAL, FIRE, WATER, ELECTRIC, GRASS,
+	FIGHTING, GROUND, FLYING, PSYCHIC, GHOST, DRAGON,
+}
+
+@export_group("Types")
+## Primary type. Must never be NONE.
+@export var type_1: Type = Type.NORMAL
+## Secondary type. NONE means single-typed.
+@export var type_2: Type = Type.NONE
+
+@export_group("Base Stats")
+@export_range(1, 255) var base_hp: int = 1
+@export_range(1, 255) var base_attack: int = 1
+@export_range(1, 255) var base_defense: int = 1
+@export_range(1, 255) var base_sp_attack: int = 1
+@export_range(1, 255) var base_sp_defense: int = 1
+@export_range(1, 255) var base_speed: int = 1
+
+@export_group("Abilities")
+@export var abilities: Array[StringName] = []
+
+@export_group("Learnset")
+@export var moves: Dictionary = {}
+
+func _init() -> void:
+	assert(type_1 != Type.NONE, "SpeciesData.type_1 must not be NONE")
